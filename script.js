893f259995685665586b59a61387e2e5cc4a76e3
joaolotjr <joaolotjr@gmail.com>
2022-08-01 15:45:32 -0300

    Script logic - Part 2

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 var jogador = null;
+var jogadorSelecionado = document.getElementById('jogador-selecionado')
 
-
+mudarjogador('X')
 
 function escolheQuadrado (id) {
-    console.log(id);
+
+    //console.log(id);
+
+    var quadrado = document.getElementById(id);
+
+    quadrado.innerHTML = jogador;
+    quadrado.style.color = '#000'
+}
+
+
+function mudarjogador(valor) {
+    jogador = valor;
+    jogadorSelecionado.innerHTML = jogador;
 }
